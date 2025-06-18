b77d79010098d8d736deeb0b56f1dee90d506d67
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-06-18 11:56:02 +0200

    Created and styled the header block included changed base css

--- acf-fields/header.php
+++ acf-fields/header.php
@@ -11,8 +11,13 @@ $header
     ->addText('pre_title', ['label' => 'Voortitel'])
     ->addText('title', ['label' => 'Titel'])
     
-    ->addColorPicker('background_color', ['label' => 'Achtergrondkleur'])
-    ->addColorPicker('text_color', ['label' => 'Tekstkleur'])
+    ->addColorPicker('background_color', [
+        'label' => 'Achtergrondkleur',
+        'default_value' => '#121212',])
+    ->addColorPicker('text_color', [
+        'label' => 'Tekstkleur',
+        'default_value' => '#FFF',
+        ])
     
     
     ->setLocation('block', '==', 'acf/header'); 
